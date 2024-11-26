260b31bc272dc543effc9bcf510f05d02d0ca75f
NakagamiYuta <guguru0014@gmail.com>
2024-11-26 16:25:55 +0900

    [add] SBLangDef, SBTokens, SBRules に Serialize, Deserialize を載せた

--- compiler/crates/parse_syntax/src/lib.rs
+++ compiler/crates/parse_syntax/src/lib.rs
@@ -1,23 +1,34 @@
+use serde::{Serialize, Deserialize};
+
 use copager::cfl::{CFL, CFLTokens, CFLRules};
 use copager::template::LALR1;
 use copager::prelude::*;
 
 pub type SBLang = LALR1<SBLangDef>;
 
-#[derive(Debug, Default, CFL)]
+#[derive(
+    Debug, Default, Clone, Copy,
+    CFL, Serialize, Deserialize,
+)]
 pub struct SBLangDef (
     #[tokens] SBTokens,
     #[rules]  SBRules,
 );
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, CFLTokens)]
+#[derive(
+    Debug, Default, Clone, Copy, PartialEq, Eq, Hash,
+    CFLTokens, Serialize, Deserialize,
+)]
 pub enum SBTokens {
     #[default]
     #[token("[0-9]+")]
     Num,
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, CFLRules)]
+#[derive(
+    Debug, Default, Clone, Copy, PartialEq, Eq, Hash,
+    CFLRules, Serialize, Deserialize,
+)]
 pub enum SBRules {
     #[default]
     #[rule("<expr> ::= Num")]
