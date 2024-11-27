9ac859e567460c920dd210473cd280048024f973
NakagamiYuta <guguru0014@gmail.com>
2024-11-27 16:18:34 +0900

    [add] analyze クレート仮作成

---
+++ compiler/crates/analyze/src/lib.rs
@@ -0,0 +1,5 @@
+use sb_compiler_parse_ast::AST;
+
+pub fn analyze(_ast: &AST) -> anyhow::Result<()> {
+    Ok(())
+}
--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,9 +1,11 @@
 use sb_compiler_parse::parse;
+use sb_compiler_analyze::analyze;
 use sb_compiler_lirgen::lirgen;
 use sb_compiler_codegen::codegen;
 
 pub fn compile(input: &str) -> anyhow::Result<Vec<String>> {
     let ast = parse(input)?;
+    let _ = analyze(&ast)?;
     let lirs = lirgen(ast);
     let asms = codegen(lirs);
     Ok(asms)
