3a952962bdb797e5b2b78f49318817f933f4a9ac
NakagamiYuta <guguru0014@gmail.com>
2024-11-27 18:42:48 +0900

    [add] 定義解析の処理を追加

--- compiler/crates/analyze/src/lib.rs
+++ compiler/crates/analyze/src/lib.rs
@@ -1,5 +1,12 @@
 use sb_compiler_parse_ast::AST;
+use sb_compiler_analyze_def::DefineTable;
 
-pub fn analyze(_ast: &AST) -> anyhow::Result<()> {
-    Ok(())
+pub struct AnalyzeResult<'ast> {
+    pub define_table: DefineTable<'ast>,
+}
+
+pub fn analyze(ast: &AST) -> anyhow::Result<AnalyzeResult> {
+    Ok(AnalyzeResult {
+        define_table: DefineTable::from(ast)?,
+    })
 }
--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -5,7 +5,7 @@ use sb_compiler_codegen::codegen;
 
 pub fn compile(input: &str) -> anyhow::Result<Vec<String>> {
     let ast = parse(input)?;
-    let _ = analyze(&ast)?;
+    let analyze_result = analyze(&ast)?;
     let lirs = lirgen(ast);
     let asms = codegen(lirs);
     Ok(asms)
