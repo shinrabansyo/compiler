33f5c7e700ed3821d4dcbdc097a510caf3c329f4
NakagamiYuta <guguru0014@gmail.com>
2024-11-27 14:44:42 +0900

    [add] 仮の cli を作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use sb_compiler::compile;
+
+fn main() -> anyhow::Result<()> {
+    let args = std::env::args();
+    if args.len() < 3 {
+        return Err(anyhow::anyhow!("usage: sb-compiler <input> <output>"));
+    }
+
+    let path = std::env::args().nth(1).unwrap();
+    let input = std::fs::read_to_string(&path)?;
+    let asms = compile(&input)?;
+
+    let path = std::env::args().nth(2).unwrap();
+    let output = asms.join("\n");
+    std::fs::write(&path, output)?;
+
+    Ok(())
 }
