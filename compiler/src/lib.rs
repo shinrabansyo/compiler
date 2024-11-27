48c77e71a07a3be27d7f722c08b8a0d59a6025b4
NakagamiYuta <guguru0014@gmail.com>
2024-11-27 14:33:00 +0900

    [add] 式のコンパイルを行えるようにした

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -4,7 +4,7 @@ use sb_compiler_codegen::codegen;
 
 pub fn compile(input: &str) -> anyhow::Result<Vec<String>> {
     let ast = parse(input)?;
-    let lir = lirgen(ast);
-    println!("{:?}", lir);
-    Ok(codegen(lir))
+    let lirs = lirgen(ast);
+    let asms = codegen(lirs);
+    Ok(asms)
 }
