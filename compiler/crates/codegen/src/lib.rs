14a636798adcff013c8fa85696820ce66a2a727f
NakagamiYuta <guguru0014@gmail.com>
2024-11-29 15:56:02 +0900

    [add] 出力アセンブリの先頭に main.global へのジャンプを追加

--- compiler/crates/codegen/src/lib.rs
+++ compiler/crates/codegen/src/lib.rs
@@ -7,6 +7,7 @@ pub fn codegen(lirs: Vec<LIR>) -> String {
 
     let mut imem = String::new();
     writeln!(&mut imem, "addi r2 = r0, 0x100").unwrap();
+    writeln!(&mut imem, "beq r0, (r0, r0) -> @main.global\n").unwrap();
     for lir in lirs {
         writeln!(&mut imem, "{}", lir).unwrap();
     }
