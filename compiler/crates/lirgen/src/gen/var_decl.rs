a3a3ab30db0f51ddc8d0d526affd2d86c216f2a5
NakagamiYuta <guguru0014@gmail.com>
2024-11-30 15:47:41 +0900

    [change] "const" -> "var"

--- compiler/crates/lirgen/src/gen/var_decl.rs
+++ compiler/crates/lirgen/src/gen/var_decl.rs
@@ -1,13 +1,13 @@
-use sb_compiler_parse_ast::ConstDecl;
+use sb_compiler_parse_ast::VarDecl;
 use sb_compiler_analyze::AnalyzeResult;
 use sb_compiler_lirgen_ir::{lir, LIR, Pop, Sw};
 
 use super::{lirgen_expr, TMP_REG, VARBASE_REG, ZERO_REG};
 
-pub fn lirgen_const_decl(lirs: &mut Vec<LIR>, const_decl: &ConstDecl, analyze_result: &AnalyzeResult) {
-    lirgen_expr(lirs, &const_decl.expr, analyze_result);
+pub fn lirgen_var_decl(lirs: &mut Vec<LIR>, var_decl: &VarDecl, analyze_result: &AnalyzeResult) {
+    lirgen_expr(lirs, &var_decl.expr, analyze_result);
 
-    let node_info = analyze_result.find(&const_decl.namespace, &const_decl.ident);
+    let node_info = analyze_result.find(&var_decl.namespace, &var_decl.ident);
     let addr = node_info.local_addr;
     let base_reg = if node_info.namespace == "global" {
         ZERO_REG
--- compiler/crates/parse_ast/src/ast/var_decl.rs
+++ compiler/crates/parse_ast/src/ast/var_decl.rs
@@ -6,14 +6,14 @@ use crate::utils::{unwrap_node, unwrap_leaf};
 use super::Expr;
 
 #[derive(Debug)]
-pub struct ConstDecl {
+pub struct VarDecl {
     pub namespace: String,
     pub ident: String,
     pub ty: String,
     pub expr: Expr,
 }
 
-impl From<(String, Tree<'_, SBLangDef>)> for ConstDecl  {
+impl From<(String, Tree<'_, SBLangDef>)> for VarDecl  {
     fn from((namespace, tree): (String, Tree<'_, SBLangDef>)) -> Self {
         let (_, mut children) = unwrap_node(tree);
 
@@ -27,6 +27,6 @@ impl From<(String, Tree<'_, SBLangDef>)> for ConstDecl  {
 
         let expr = Expr::from((namespace.clone(), children.pop_front().unwrap()));
 
-        ConstDecl { namespace, ident, ty, expr }
+        VarDecl { namespace, ident, ty, expr }
     }
 }
