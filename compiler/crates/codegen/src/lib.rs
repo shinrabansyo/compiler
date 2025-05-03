921697b8e0ff969a3a9eae88beb68714cea14f0d
NakagamiYuta <guguru0014@gmail.com>
2025-05-04 00:35:46 +0900

    [add] レジスタ割り付けの結果生じた冗長コードを削減するようにした

--- compiler/crates/codegen/src/lib.rs
+++ compiler/crates/codegen/src/lib.rs
@@ -1,10 +1,12 @@
 mod asmgen;
+mod cleaning;
 mod reg_mapping;
 
 use sb_compiler_lirgen_ir::LirTree;
 use sb_compiler_codegen_asm::Asm;
 
 use asmgen::asmgen;
+use cleaning::cleaning;
 use reg_mapping::mapping;
 
 pub fn codegen(lir_tree: LirTree) -> Asm {
@@ -13,6 +15,9 @@ pub fn codegen(lir_tree: LirTree) -> Asm {
 
     // 2. コード生成 (LirTree + RegMap -> Asm)
     let asm = asmgen(lir_tree, reg_map);
+
+    // 3. 冗長コード削減 (Asm -> Asm)
+    let asm = cleaning(asm);
 
     asm
 }
