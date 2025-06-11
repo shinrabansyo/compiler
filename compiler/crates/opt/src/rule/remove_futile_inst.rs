4df332adb27643a6167e9c35d61b18c2bb52dce5
NakagamiYuta <guguru0014@gmail.com>
2025-06-11 17:47:49 +0900

    [fix] 不正な最適化を修正

--- compiler/crates/opt/src/rule/remove_futile_inst.rs
+++ compiler/crates/opt/src/rule/remove_futile_inst.rs
@@ -6,9 +6,7 @@ pub fn remove_futile_inst(mut obj: Object) -> Object {
         match inst {
             // R-形式 (rs1 = r0)
             Inst::Add { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::Sub { rd, rs1: 0, rs2 } if rd == rs2 => false,
             Inst::And { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::Or  { rd, rs1: 0, rs2 } if rd == rs2 => false,
             Inst::Xor { rd, rs1: 0, rs2 } if rd == rs2 => false,
             Inst::Sll { rd, rs1: 0, rs2 } if rd == rs2 => false,
             Inst::Srl { rd, rs1: 0, rs2 } if rd == rs2 => false,
@@ -17,7 +15,6 @@ pub fn remove_futile_inst(mut obj: Object) -> Object {
             // R-形式 (rs2 = r0)s
             Inst::Add { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Sub { rd, rs1, rs2: 0 } if rd == rs1 => false,
-            Inst::And { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Or  { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Xor { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Sll { rd, rs1, rs2: 0 } if rd == rs1 => false,
