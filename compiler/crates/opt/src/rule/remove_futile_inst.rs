9d44322b914f6adcaa6f4a38a0dd1bc23d779981
NakagamiYuta <guguru0014@gmail.com>
2025-06-11 17:49:25 +0900

    [fix] 不正な最適化を修正2

--- compiler/crates/opt/src/rule/remove_futile_inst.rs
+++ compiler/crates/opt/src/rule/remove_futile_inst.rs
@@ -6,13 +6,10 @@ pub fn remove_futile_inst(mut obj: Object) -> Object {
         match inst {
             // R-形式 (rs1 = r0)
             Inst::Add { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::And { rd, rs1: 0, rs2 } if rd == rs2 => false,
+            Inst::Or  { rd, rs1: 0, rs2 } if rd == rs2 => false,
             Inst::Xor { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::Sll { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::Srl { rd, rs1: 0, rs2 } if rd == rs2 => false,
-            Inst::Sra { rd, rs1: 0, rs2 } if rd == rs2 => false,
 
-            // R-形式 (rs2 = r0)s
+            // R-形式 (rs2 = r0)
             Inst::Add { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Sub { rd, rs1, rs2: 0 } if rd == rs1 => false,
             Inst::Or  { rd, rs1, rs2: 0 } if rd == rs1 => false,
