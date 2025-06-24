fe2bdfc342377633b1a31fd5d1cb7184a854d43e
NakagamiYuta <guguru0014@gmail.com>
2025-06-24 21:06:02 +0900

    [update] VarDeclError の miette 表示を整えた

--- compiler/crates/semcheck_impl_vardecl/src/error.rs
+++ compiler/crates/semcheck_impl_vardecl/src/error.rs
@@ -4,13 +4,18 @@ use miette::{Diagnostic, SourceSpan};
 use sb_compiler_parse_cst::Span;
 
 #[derive(Debug, Error, Diagnostic)]
+#[diagnostic(
+    code(semantics::variable::declaration),
+    url("this link is not working yet"),
+)]
 pub enum VarDeclError {
-    #[error("Variable is not declared in this scope.")]
+    #[error("Variable '{name}' is not declared in this scope.")]
     NotDeclared {
         #[source_code]
         src: String,
         #[label("here")]
         span: SourceSpan,
+        name: String,
     },
 }
 
@@ -19,6 +24,7 @@ impl VarDeclError {
         VarDeclError::NotDeclared {
             src: span.src().to_string(),
             span: (span.body.0, span.body.1-span.body.0).into(),
+            name: span.as_str().to_string(),
         }.into()
     }
 }
