0742aa8e82b220338981cf1753f4d7f3605d167f
NakagamiYuta <guguru0014@gmail.com>
2025-06-24 21:11:12 +0900

    [remove] SpanOwned 削除

--- compiler/crates/parse_cst/src/lib.rs
+++ compiler/crates/parse_cst/src/lib.rs
@@ -2,6 +2,6 @@ mod span;
 mod tree;
 mod visitor;
 
-pub use span::{Span, SpanOwned, Spanned};
+pub use span::{Span, Spanned};
 pub use tree::CSTree;
 pub use visitor::CSTreeVisitor;
--- compiler/crates/parse_cst/src/span.rs
+++ compiler/crates/parse_cst/src/span.rs
@@ -1,9 +1,59 @@
-mod borrowed;
-mod owned;
+use std::fmt::{Debug, Display};
 
-pub use borrowed::Span;
-pub use owned::SpanOwned;
+use copager::cfl::token::Token;
+use copager::cfl::token::TokenTag;
+use miette::SourceSpan;
 
 pub trait Spanned<'src> {
     fn span(&self) -> Span<'src>;
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Span<'a> {
+    pub src: &'a str,
+    pub body: (usize, usize),   // Trivia を含まない
+    pub full: (usize, usize),   // Trivia を含む
+}
+
+impl Debug for Span<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl Display for Span<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl<'a, T> From<Token<'a, T>> for Span<'a>
+where
+    T: TokenTag,
+{
+    fn from(token: Token<'a, T>) -> Self {
+        Span {
+            src: token.src,
+            body: token.body,
+            full: token.full,
+        }
+    }
+}
+
+impl<'a> Into<SourceSpan> for Span<'a> {
+    fn into(self) -> SourceSpan {
+        (self.body.0, self.body.1 - self.body.0).into()
+    }
+}
+
+impl<'a> Span<'a> {
+    pub fn as_str(&self) -> &'a str {
+        let (l, r) = self.body;
+        &self.src[l..r]
+    }
+
+    pub fn as_full_str(&self) -> &'a str {
+        let (l, r) = self.full;
+        &self.src[l..r]
+    }
 }
