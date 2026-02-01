97599b32486dd5dc2208f669e6853997130baddc
NakagamiYuta <guguru0014@gmail.com>
2026-02-01 17:58:05 +0900

    [fix] 変数宣言時の右辺が関数呼び出しだった時に，初期化が行われないバグを修正

--- compiler/crates/lirgen/src/gen/var_decl.rs
+++ compiler/crates/lirgen/src/gen/var_decl.rs
@@ -1,17 +1,31 @@
-use sb_compiler_lirgen_ir::LirBlock;
+use sb_compiler_lirgen_ir::{lir, Add, LirBlock};
 use sb_compiler_semcheck_hir::VarDecl;
 
 use crate::GenContext;
-use super::lirgen_expr;
+use super::{ZERO_REG, RET_REG, lirgen_expr};
 
 pub fn lirgen_var_decl<'src>(ctx: &mut GenContext<'src>, var_decl: VarDecl<'src>) -> LirBlock {
     let lir_expr = lirgen_expr(ctx, var_decl.expr);
     let reg_expr = lir_expr.result_reg();
 
-    ctx.set_var_reg(var_decl.var, reg_expr);
+    let lir_var = if reg_expr == RET_REG {
+        let reg_var = ctx.alloc_reg();
+        LirBlock::Single {
+            result_reg: reg_var,
+            lirs: vec![
+                lir!(Add reg_var, ZERO_REG, RET_REG),
+            ],
+        }
+    } else {
+        LirBlock::Single { result_reg: reg_expr, lirs: vec![] }
+    };
+    ctx.set_var_reg(var_decl.var, lir_var.result_reg());
 
     LirBlock::Single {
         result_reg: reg_expr,
-        lirs: vec![lir_expr],
+        lirs: vec![
+            lir_expr,
+            lir_var,
+        ],
     }
 }
